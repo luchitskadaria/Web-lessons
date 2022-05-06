a2575a76f8fed6906ba983ad423269049e313a1f
luchitskadaria <93514263+luchitskadaria@users.noreply.github.com>
2022-05-06 17:07:21 +0300

    Create script.js

---
+++ 10/script.js
@@ -0,0 +1,13 @@
+function Doc(){
+    document.write(123);
+
+    document.write('<br><i>Italic</i>')
+
+    let str = 'text'
+    document.write('<br>'+'<i>' +str + '</i>')
+
+    for (let i = 1; i <= 5; i++) {
+        document.write('<br>'+i)
+      }
+      
+    }
