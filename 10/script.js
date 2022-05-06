7551568b634133d7e860f60086d22cdd2711e869
luchitskadaria <93514263+luchitskadaria@users.noreply.github.com>
2022-05-06 17:11:33 +0300

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -1,11 +1,15 @@
 function Doc(){
+    document.write("Выведите в окно браузера какой-нибудь текст. <br>")
     document.write(123);
 
+    document.write("<br>Выведите в окно браузера какой-нибудь текст, сделав его курсивным.")
     document.write('<br><i>Italic</i>')
 
-    let str = 'text'
+    document.write("<br>Выведите текст из переменной в окно браузера так, чтобы он был курсивным.")
+    let str = prompt();
     document.write('<br>'+'<i>' +str + '</i>')
 
+    document.write("<br>Выведите в окно браузера столбец чисел от 1 до 5.")
     for (let i = 1; i <= 5; i++) {
         document.write('<br>'+i)
       }
